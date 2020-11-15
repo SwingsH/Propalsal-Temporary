738351ee283b37274e5ea8ee7ce3052c35e71bbf
SwingsHuang <silverwings999@gmail.com>
2020-11-15 22:53:11 +0800

    update some anim

---
+++ unity-helmet-prototype/Assets/Scripts/GameMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    ChooseCommand,
+    PlayCommand,
+    ShowCommandResault,
+    ReadyToNextTurn
+}
+
+public class GameMain : MonoBehaviour
+{
+    public Animator playerAnimator;
+    protected GameState gameState = GameState.ChooseCommand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator.Play("Battle_Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
